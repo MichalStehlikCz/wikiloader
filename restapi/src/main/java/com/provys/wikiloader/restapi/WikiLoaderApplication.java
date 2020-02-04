0385fd2708d5e41b5ebb7f0089a6c477ce4cd905
Michal Stehlik <stehlik@provys.com>
2020-02-04 18:25:06 +0100

    Limit endpoints offered in swagger

--- restapi/src/main/java/com/provys/wikiloader/restapi/WikiLoaderApplication.java
+++ restapi/src/main/java/com/provys/wikiloader/restapi/WikiLoaderApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,6 +19,17 @@ public class WikiLoaderApplication {
 
         public static void main(String[] args) {
                 SpringApplication.run(WikiLoaderApplication.class, args);
+        }
+
+        @Bean
+        public Docket petApi() {
+                return new Docket(DocumentationType.SWAGGER_2)
+                        .select()
+                        .apis(RequestHandlerSelectors.basePackage("com.provys"))
+                        .build()
+                        .tags(new Tag("WikiLoader", "Apis allowing loading information from " +
+                                "Enterprise Architect to Provys wiki"))
+                        ;
         }
 
         @Bean
